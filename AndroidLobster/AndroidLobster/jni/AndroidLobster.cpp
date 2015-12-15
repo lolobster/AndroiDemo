697b06f59f412ec787e6496c575e43e62ad46906
lolobster <nissinena@elisanet.fi>
2015-12-15 22:06:37 +0200

    kolomio ja mikälie karvakasa

--- AndroidLobster/AndroidLobster/jni/AndroidLobster.cpp
+++ AndroidLobster/AndroidLobster/jni/AndroidLobster.cpp
@@ -31,10 +31,16 @@ static const char gVertexShader[] =
 	"	gl_Position = vPosition;\n"
 	"}\n";
 
-static const char gFragmentShader[] =
+static const char gFragmentShader[] =  // SININEN
 "precision mediump float;\n"
 "void main() {\n"
-"	gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+"	gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);\n"
+"}\n";
+
+static const char gFragmentShader2[] =  // LIILA
+"precision mediump float;\n"
+"void main() {\n"
+"	gl_FragColor = vec4(0.7, 0.0, 1.0, 1.0);\n"
 "}\n";
 
 GLuint loadShader(GLenum shaderType, const char* pSource)
@@ -121,8 +127,10 @@ GLuint createProgram(const char* pVertexSource, const char* pFragmentSource)
 	return program;
 }
 
-GLuint gProgram;
-GLuint gvPositionHandle;
+GLuint gProgramBlue;
+GLuint gProgramLila;
+GLuint gvPositionHandleBlue;
+GLuint gvPositionHandleLila;
 
 bool setupGraphics(int w, int h)
 {
@@ -131,16 +139,29 @@ bool setupGraphics(int w, int h)
 	printGLString("Renderer", GL_RENDERER);
 	printGLString("Extensions", GL_EXTENSIONS);
 
+
+	//////////////////////////// eka shader
 	LOGI("setupGraphics(%d, %d)", w, h);
-	gProgram = createProgram(gVertexShader, gFragmentShader);
-	if (!gProgram)
+	gProgramBlue = createProgram(gVertexShader, gFragmentShader);
+	if (!gProgramBlue)
 	{
 		LOGE("Could not create program.");
 		return false;
 	}
-	gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");
+	gvPositionHandleBlue = glGetAttribLocation(gProgramBlue, "vPosition");
 	checkGlError("glGetAttribLocation");
-	LOGI("glGetAttribLocation(\"vPosition\") = %d\n", gvPositionHandle);
+	LOGI("glGetAttribLocation(\"vPosition\") = %d\n", gvPositionHandleBlue);
+
+	/////////////////// toka shader
+	gProgramLila = createProgram(gVertexShader, gFragmentShader2);
+	if (!gProgramLila)
+	{
+		LOGE("Could not create program.");
+		return false;
+	}
+	gvPositionHandleLila = glGetAttribLocation(gProgramLila, "vPosition");
+	checkGlError("glGetAttribLocation");
+	LOGI("glGetAttribLocation(\"vPosition\") = %d\n", gvPositionHandleLila);
 
 	glViewport(0, 0, w, h);
 	checkGlError("glViewPort");
@@ -148,28 +169,55 @@ bool setupGraphics(int w, int h)
 }
 
 const GLfloat gTriangleVertices[] = {
-	0.0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f
+	0.0f, 0.5f, -0.5f,
+	-0.5f, 0.5f, -0.5f
 };
 
 void renderFrame()
 {
-	static float grey;
-	grey += 0.01f;
-	if (grey > 1.0f)
-	{
-		grey = 0.0f;
-	}
-	glClearColor(grey, grey, grey, 1.0f);
+	static float angle = 0.01f;
+	GLfloat PI = 3.1415926;
+
+	glClearColor(0.2, 0.2, 0.2, 1.0f);
 	checkGlError("glClearColor");
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 	checkGlError("glClear");
 
-	glUseProgram(gProgram);
+	{
+		glUseProgram(gProgramLila);
+		checkGlError("glUseProgram");
+
+		GLfloat vertexBuffer[6 * 100 * 6];
+		
+		for (int i = 0; i <= 100; i++)
+		{
+			float angle2 = 2* PI * i / 100;
+			float a = PI / 100;
+			const float gTriangleVertices[] =
+			{
+				cos(angle2)*0.05, sin(angle2)*0.05,
+				cos(angle2 + a - 0.025)*0.9, sin(angle2 + a - 0.025)*0.9,
+				cos(angle2 - a + 0.025)*0.9, sin(angle2 - a + 0.025)*0.9,
+			};
+			for (int j = 0; j < 6; j++)
+			{
+				vertexBuffer[j + i * 6] = gTriangleVertices[j];
+			}
+		}
+		glVertexAttribPointer(gvPositionHandleLila, 2, GL_FLOAT, GL_FALSE, 0, vertexBuffer);
+		checkGlError("glVertexAttribPointer");
+		glEnableVertexAttribArray(gvPositionHandleLila);
+		checkGlError("glEnableVertexAttribArray");
+		glDrawArrays(GL_TRIANGLES, 0, 3 * 100);
+		checkGlError("glDrawArrays");
+	};
+
+	glUseProgram(gProgramBlue);
 	checkGlError("glUseProgram");
 
-	glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
+	glVertexAttribPointer(gvPositionHandleLila, 2, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
 	checkGlError("glVertexAttribPointer");
-	glEnableVertexAttribArray(gvPositionHandle);
+	glEnableVertexAttribArray(gvPositionHandleLila);
 	checkGlError("glEnableVertexAttribArray");
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	checkGlError("glDraw Arrays");
